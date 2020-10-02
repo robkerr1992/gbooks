a7ec34d69ee623261cba786349263f4fb96cfb31
Robert Kerr <robert.kerr@elevationeg.com>
2020-10-01 21:49:36 -0500

    gbooks config file

--- config/gbooks.php
+++ config/gbooks.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
+    /**
+     * Google Services simple API access key from the API console
+     * For whatever reason they call it a developer_key
+     */
 
+    'developer_key' => env('GOOGLE_BOOKS_DEVELOPER_KEY', ''),
 ];
