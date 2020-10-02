66cf2a726e23eee89914c04ef968bfd0591f2d34
Robert Kerr <robert.kerr@elevationeg.com>
2020-10-01 21:52:58 -0500

    v1, does searching for volumes based on all the different possible filters, can search for a volume by id, and can search for similar volumes by id

--- src/Gbooks.php
+++ src/Gbooks.php
@@ -4,7 +4,27 @@ namespace Rksugarfree\Gbooks;
 
 class Gbooks
 {
-    public function __construct()
+    use SearchTerms;
+
+    private \Google_Service_Books $googleServiceBooks;
+
+    public function __construct(\Google_Service_Books $googleServiceBooks)
     {
+        $this->googleServiceBooks = $googleServiceBooks;
+    }
+
+    public function byVolumeId(string $volumeId): \Google_Service_Books_Volume
+    {
+        return $this->googleServiceBooks->volumes->get($volumeId);
+    }
+
+    public function get(): \Google_Service_Books_Volumes
+    {
+        return $this->googleServiceBooks->volumes->listVolumes($this->queryParams);
+    }
+
+    public function similar(string $volumeId): \Google_Service_Books_Volumes
+    {
+        return $this->googleServiceBooks->volumes_associated->listVolumesAssociated($volumeId);
     }
 }
